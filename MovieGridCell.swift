8eb0336c441a9bd3ba66e1a8e63ec67b13f7898e
NickAFong <nickfo@rocketmail.com>
2020-09-12 17:00:22 -0700

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  flixter
+//
+//  Created by Nick Fong on 9/10/20.
+//  Copyright © 2020 Nick Fong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
